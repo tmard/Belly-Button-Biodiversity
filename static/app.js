628b55ac07dda40ae52aca6c581c68c6fc39f4bb
Tiara Mardosas <temardosas@gmail.com>
2023-04-05 19:53:40 -0400

    Update file

--- static/app.js
+++ static/app.js
@@ -171,7 +171,12 @@ function AllCharts(data, subjectId) {
   let gaugeLayout = { 
       width: 500, 
       height: 400,
-      margin: {t: 0, r: 0, l: 0, b: 0}
+      margin: {
+        l: 0,
+        r: 0,
+        b: 0,
+        t: 0   
+      }
     };
 
   // Use Plotly to plot the gauge data and layout
