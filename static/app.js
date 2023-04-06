85fdfc639940dd8fddfe0a914734f7440836b19a
Tiara Mardosas <temardosas@gmail.com>
2023-04-06 17:43:52 -0400

    Update code

--- static/app.js
+++ static/app.js
@@ -1,23 +1,5 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Create function to create dropdown menu
-function init(){
-  d3.json(url).then(data => {
-
-    let subjectIds = data.names;
-    let select = d3.select("#selDataset");
-
-    subjectIds.forEach(id => {
-      select.append("option").attr("value", id).text(id);
-    });
-    
-    redraw(subjectIds[0]);
-  });
-}
-
-
-init();
-
 
 // Demographic Info
 function DemoInfo(data, subjectId) {
@@ -170,6 +152,26 @@ function AllCharts(data, subjectId) {
   // Use Plotly to plot the gauge data and layout
   Plotly.newPlot("gauge", gaugeData, gaugeLayout);
 }
+
+
+// Create function to create dropdown menu
+function init(){
+  d3.json(url).then(data => {
+
+    let subjectIds = data.names;
+    let select = d3.select("#selDataset");
+
+    subjectIds.forEach(id => {
+      select.append("option").attr("value", id).text(id);
+    });
+    
+    redraw(subjectIds[0]);
+  });
+}
+
+
+init();
+
 
 // Create function to redraw data, subject ID and below charts for each Subject ID selected in the dropdown menu
 function redraw(subjectId) {
